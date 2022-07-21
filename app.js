4656914185232d04124d096963825be3d05d0aaf
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-07-21 16:31:05 +0530

    to check

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 
 app.use('/', (req, res) => {
-    res.send("Welcome to the home page...!");
+    res.send("Welcome to the Deployed page...!");
 })
 
 const PORT = process.env.PORT || 3000
