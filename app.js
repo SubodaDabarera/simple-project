79795b6fbd83a4aa356196eb90b5cd365cafc7be
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-07-21 16:25:37 +0530

    some changes added

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express')
 
 const app = express();
 
-
-
 app.use('/', (req, res) => {
     res.send("Welcome to the home page...!");
 })
@@ -13,3 +11,5 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log("App is running on ", PORT)
 })
+
+module.exports = app;
